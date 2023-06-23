86ff6b36de88222e98c6d05d68d451f931d42f48
Taguine Tarek <t.taguine@it-students.fr>
2023-06-23 14:56:56 +0200

    feature/createStructure
    Create structure with Twig

--- src/Controller/AboutController.php
+++ src/Controller/AboutController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class AboutController extends AbstractController
+class AboutController extends AbstractController // controller de la page A propos
 {
     #[Route('/about', name: 'app_about')]
     public function index(): Response
