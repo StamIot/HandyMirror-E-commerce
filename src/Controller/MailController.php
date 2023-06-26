4282b7bfb099dafcc2afe837bd55e3d8268659f9
Alain Guillon <zyrass@outlook.fr>
2023-06-26 13:52:11 +0200

    feature: test conflict
    create a conflict for test

--- src/Controller/MailController.php
+++ src/Controller/MailController.php
@@ -4,20 +4,32 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Mime\Email;
 
 class MailController extends AbstractController
 {
+    #[Route('/send-email', name: 'send_email')]
     public function sendEmail(MailerInterface $mailer)
     {
-        $email = (new Email())
-            ->from('handymirrorproject@gmail.com')
-            ->to('sophiemajorel@hotmail.fr')
-            ->subject('RECLAMATION - 2022 ')
-            ->text('BONSOUAAAR PARIS YEEEAAH.');
+        try {
+            $email = (new Email())
+                ->from('handymirrorproject@gmail.com')
+                ->to('sophiemajorel@hotmail.fr')
+                ->subject('RECLAMATION - 2022 ')
+                ->text('BONSOUAAAR PARIS YEEEAAH.');
 
-        $mailer->send($email);
+            $mailer->send($email);
 
-        return $this->render('email/sent.html.twig');
+            return $this->render('email/sent.html.twig');
+        } catch (\Exception $e) {
+            // Gérer l'exception ici
+            $errorMessage = 'Une erreur s\'est produite lors de l\'envoi de l\'e-mail : ' . $e->getMessage();
+            // Vous pouvez ajouter d'autres actions à effectuer en cas d'erreur, telles que l'enregistrement de l'erreur dans un journal ou l'affichage d'un message d'erreur personnalisé.
+
+            return $this->render('email/error.html.twig', [
+                'errorMessage' => $errorMessage,
+            ]);
+        }
     }
 }
