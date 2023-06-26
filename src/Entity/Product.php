1f127d5a2cd2058b390f20e8935e7af0ee0ebfa6
Taguine Tarek <t.taguine@it-students.fr>
2023-06-26 16:06:06 +0200

    hotfix: Product
    Correction product tables

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -22,6 +22,12 @@ class Product
     #[ORM\Column(length: 255)]
     private ?string $description = null;
 
+    #[ORM\Column(length: 50)]
+    private ?string $title = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $image = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -59,6 +65,30 @@ class Product
     public function setDescription(string $description): static
     {
         $this->description = $description;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): static
+    {
+        $this->image = $image;
 
         return $this;
     }
