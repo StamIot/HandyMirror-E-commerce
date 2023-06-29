94943b3edccbfc251d0231c3b11ef92c384157cf
wikkly <sophiemajorel@hotmail.fr>
2023-06-29 14:51:18 +0200

    testlogin

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Form\LogInFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -10,20 +12,31 @@ use Symfony\Component\Security\Http\RememberMe\TokenBasedRememberMeServices;
 
 class SecurityController extends AbstractController
 {
-    #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    /**
+     * @Route("/login", name="app_login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils, Request $request): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+        $form = $this->createForm(LogInFormType::class);
+        $form->handleRequest($request);
+
+        $lastUsername = $authenticationUtils->getLastUsername();    
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
-        $lastUsername = $authenticationUtils->getLastUsername();
+        // last username entered by the user
+        // last username entered by the user
+        return $this->render('security/login_form.html.twig', [
+            'form' => $form->createView(),
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
-
+    /**
+     * @Route("/logout", name="app_logout")
+     */
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
     {
--- src/Form/LogInFormType.php
+++ src/Form/LogInFormType.php
@@ -5,6 +5,8 @@ namespace App\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+
 use Symfony\Component\Form\FormBuilderInterface;
 
 class LogInFormType extends AbstractType
@@ -25,6 +27,10 @@ class LogInFormType extends AbstractType
                 'attr' => [
                     'placeholder' => "Veuillez saisir votre mot de passe ici..."
                 ]
-            ]);
+            ])
+            ->add('_remember_me', CheckboxType::class, [
+                'label' => 'Se souvenir de moi',
+                'required' => false,
+            ]);;
     }
 }
