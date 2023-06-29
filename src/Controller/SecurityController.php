297576dad3f9066e84ce91e2e9dd509219fa7d4b
wikkly <sophiemajorel@hotmail.fr>
2023-06-29 17:27:32 +0200

    Feature/forgotpassword
    Add the feature forgotpassword and resetpassword, the mail still don't work.

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,9 +2,7 @@
 
 namespace App\Controller;
 
-use App\Form\LogInFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -12,30 +10,20 @@ use Symfony\Component\Security\Http\RememberMe\TokenBasedRememberMeServices;
 
 class SecurityController extends AbstractController
 {
-    /**
-     * @Route("/login", name="app_login")
-     */
-    public function login(AuthenticationUtils $authenticationUtils, Request $request): Response
+    #[Route(path: '/login', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        $form = $this->createForm(LogInFormType::class);
-        $form->handleRequest($request);
-
-        $lastUsername = $authenticationUtils->getLastUsername();    
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
-        return $this->render('security/login_form.html.twig', [
-            'form' => $form->createView(),
-            'last_username' => $lastUsername,
-            'error' => $error,
-        ]);
+        $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login_form.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
-    /**
-     * @Route("/logout", name="app_logout")
-     */
+
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
     {
