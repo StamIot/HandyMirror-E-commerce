44d265ce553c6f390c3a955857b0e52783c23750
Alain Guillon <zyrass@outlook.fr>
2023-06-29 14:32:32 +0200

    patch: logs
    patch logs

--- src/Service/HandyLogs.php
+++ src/Service/HandyLogs.php
@@ -16,27 +16,27 @@ class HandyLogs
     {
         $status = $this->_status[0];
         $contentInfo = "ℹ️ - " . date("d-m-Y H:i:s") . " - $status - $message" . PHP_EOL;
-        $this->fs->appendToFile('log/handyLogs.log', $contentInfo);
+        $this->fs->appendToFile('logs/handyLogs.log', $contentInfo);
     }
 
     public function writeWarning($message)
     {
         $status = $this->_status[1];
         $contentInfo = "🐛 - " . date("d-m-Y H:i:s") . " - $status - $message" . PHP_EOL;
-        $this->fs->appendToFile('log/handyLogs.log', $contentInfo);
+        $this->fs->appendToFile('logs/handyLogs.log', $contentInfo);
     }
 
     public function writeError($message)
     {
         $status = $this->_status[2];
         $contentInfo = "❌ - " . date("d-m-Y H:i:s") . " - $status - $message" . PHP_EOL;
-        $this->fs->appendToFile('log/handyLogs.log', $contentInfo);
+        $this->fs->appendToFile('logs/handyLogs.log', $contentInfo);
     }
 
     public function writeSuccess($message)
     {
         $status = $this->_status[3];
         $contentInfo = "✅ - " . date("d-m-Y H:i:s") . " - $status - $message" . PHP_EOL;
-        $this->fs->appendToFile('log/handyLogs.log', $contentInfo);
+        $this->fs->appendToFile('logs/handyLogs.log', $contentInfo);
     }
 }
