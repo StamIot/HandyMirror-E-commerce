93409d1156eb098439a9094a1d84b0b31ad9f960
wikkly <sophiemajorel@hotmail.fr>
2023-07-03 16:03:08 +0200

    Add the scss of the user page

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -11,8 +11,34 @@ class AccountController extends AbstractController
     #[Route('/account', name: 'app_account')]
     public function index(): Response
     {
+        $user = $this->getUser();
+        $command = "Aucune commande pour le moment";
+
+        $firstName = $user->getFirstname();
+        $lastName = $user->getLastname();
+        $email = $user->getEmail();
+
+        // Définissez un tableau d'URLs d'images de profil
+        $profileImages = [
+            'images/icon1.png',
+            'images/icon2.png',
+            'images/icon3.png',
+            'images/icon4.png',
+            'images/icon5.png',
+            'images/icon6.png',
+            'images/icon7.png',
+        ];
+
+        // Choisissez aléatoirement une URL d'image de profil parmi le tableau
+        $randomProfileImage = $profileImages[array_rand($profileImages)];
+
         return $this->render('account/index.html.twig', [
             'titleH1' => 'Mon Compte client',
+            'profileImageUrl' => $randomProfileImage,
+            'firstName' => $firstName,
+            'lastName' => $lastName,
+            'email' => $email,
+            'command' => $command,
         ]);
     }
 }
