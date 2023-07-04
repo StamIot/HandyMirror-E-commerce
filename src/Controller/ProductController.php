df65fc2470bc3900d8667062cc0f85d95dcd4d32
Alain Guillon <zyrass@outlook.fr>
2023-07-04 23:21:33 +0200

    hotfix: euro
    hotfix show euro

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Product;
 use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,15 +12,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProductController extends AbstractController
 {
-
     #[Route('/product/{id}', name: 'app_product')]
     public function index(Request $request, ProductRepository $productRepository): Response
     {
         return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
-             'titleH1' => 'Produit',
-             'product' => $productRepository->find($request->get('id')),
-             
+            'titleH1' => 'Produit',
+            'product' => $productRepository->find($request->get('id'))
         ]);
     }
 }
