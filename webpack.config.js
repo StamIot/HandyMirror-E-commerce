da112d9132dac1a66bc74a42412ae8b511f5fa9f
Alain Guillon <zyrass@outlook.fr>
2023-07-11 08:26:44 +0200

    feature: Refactoring good practice scss
    cart OK

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
-const Encore = require('@symfony/webpack-encore');
+const Encore = require('@symfony/webpack-encore')
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
 if (!Encore.isRuntimeEnvironmentConfigured()) {
-    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev')
 }
 
 Encore
@@ -25,6 +25,7 @@ Encore
     .addEntry('homePage', './assets/homePage.js')
     .addEntry('productList', './assets/productList.js')
     .addEntry('productDetails', './assets/productDetails.js')
+    .addEntry('cart', './assets/cart.js')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
@@ -53,25 +54,24 @@ Encore
 
     // enables and configure @babel/preset-env polyfills
     .configureBabelPresetEnv((config) => {
-        config.useBuiltIns = 'usage';
-        config.corejs = '3.23';
+        config.useBuiltIns = 'usage'
+        config.corejs = '3.23'
     })
 
     // enables Sass/SCSS support
     .enableSassLoader()
 
-    // uncomment if you use TypeScript
-    //.enableTypeScriptLoader()
+// uncomment if you use TypeScript
+//.enableTypeScriptLoader()
 
-    // uncomment if you use React
-    //.enableReactPreset()
+// uncomment if you use React
+//.enableReactPreset()
 
-    // uncomment to get integrity="..." attributes on your script & link tags
-    // requires WebpackEncoreBundle 1.4 or higher
-    //.enableIntegrityHashes(Encore.isProduction())
+// uncomment to get integrity="..." attributes on your script & link tags
+// requires WebpackEncoreBundle 1.4 or higher
+//.enableIntegrityHashes(Encore.isProduction())
 
-    // uncomment if you're having problems with a jQuery plugin
-    //.autoProvidejQuery()
-;
+// uncomment if you're having problems with a jQuery plugin
+//.autoProvidejQuery()
 
-module.exports = Encore.getWebpackConfig();
+module.exports = Encore.getWebpackConfig()
