2691529542326f6d48d08f1bcc0f5d5b68ccc241
Alain Guillon <zyrass@outlook.fr>
2023-07-11 08:23:07 +0200

    feature: Refactoring good practice scss
    Base, homepage, productList and productDetails OK

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,11 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/app.js')
+    // .addEntry('app', './assets/app.js')
+    .addEntry('base', './assets/base.js')
+    .addEntry('homePage', './assets/homePage.js')
+    .addEntry('productList', './assets/productList.js')
+    .addEntry('productDetails', './assets/productDetails.js')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
